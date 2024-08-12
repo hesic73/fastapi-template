a9c199a8463c3f2c06c31ecdbf3efbf0b9c8bf74
Sicheng He <hesicheng20@gmail.com>
2024-08-11 23:29:01 -0700

    minor update

--- app/database/models/user.py
+++ app/database/models/user.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, String, Enum
+from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy_utils.types.email import EmailType
+
+from pydantic import EmailStr
 
 from ..base import Base
 
@@ -8,11 +11,14 @@ from app.enums import UserType
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String, nullable=False, unique=True)
-    hashed_password = Column(String, nullable=False)
-    email = Column(EmailType, nullable=False, unique=True)
-    user_type = Column(Enum(UserType), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(
+        String(30), nullable=False, unique=True)
+    hashed_password: Mapped[str] = mapped_column(
+        String(30), nullable=False)
+    email: Mapped[EmailStr] = mapped_column(
+        EmailType, nullable=False, unique=True)
+    user_type: Mapped[UserType] = mapped_column(Enum(UserType), nullable=False)
 
     def __repr__(self):
         return f"{self.username}"
